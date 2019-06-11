be35d4c3a0605a2a6858a3b44a47786273b34511
Andrew Wester <awester17@gmail.com>
2019-06-11 15:19:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,5 +31,9 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-
+  if (age >= 13 && age <= 19) {
+    return "You are a teenager"
+  } else {
+    return "You have an age"
+  }
 }
